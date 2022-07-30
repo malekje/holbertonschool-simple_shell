217a5feb9774a249babed97aef8863e87b8e43c2
Malek Mayeh <loukatankionline@gmail.com>
2022-07-30 15:11:27 +0100

    header file

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+        
+#endif 
